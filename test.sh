6dbcb09d67d9f7b329e99e8ab096d4afc6f64647
dlqs <donaldleeqs@gmail.com>
2019-07-08 19:17:27 +0800

    change env var name to match

--- test.sh
+++ test.sh
@@ -2,14 +2,14 @@
 set -e
 DATE=$(date)
 BUCKET_NAME='opendoc-theme-pdf'
-if [[ -z "${LAMBDA_API_KEY}" ]]; then
-  echo "Missing environment variable LAMBDA_API_KEY; exiting.";
+if [[ -z "${PDF_GEN_API_KEY}" ]]; then
+  echo "Missing environment variable PDF_GEN_API_KEY; exiting.";
   exit
 fi
 curl -X POST \
   'https://4c08rjv4pa.execute-api.ap-southeast-1.amazonaws.com/prod/create_pdf_2' \
   -H "content-type: application/json" \
-  -H "x-api-key: ${LAMBDA_API_KEY}" \
+  -H "x-api-key: ${PDF_GEN_API_KEY}" \
   -d '{ "serializedHTML":"<!DOCTYPE html><p>If you are seeing this, the PDF was generated successfully at '"$DATE"'. Awesome!</p>","serializedHTMLName":"hello.pdf","serializedHTMLHash":"ZmJlZTVkYzRiNDNmYmUyYzNhNWM5OGFlODc4ZDJjNmM=","bucketName":"'$BUCKET_NAME'"}'
 
 echo -e "\n\nCheck for hello.pdf at https://$BUCKET_NAME.s3-ap-southeast-1.amazonaws.com/hello.pdf"
